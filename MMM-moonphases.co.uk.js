60e49d087ca97903f9e149ccc02c75f779a194ec
Tony Morris <tmorris@tmorris.net>
2018-12-20 21:33:48 +1000

    size

--- MMM-moonphases.co.uk.js
+++ MMM-moonphases.co.uk.js
@@ -1,8 +1,8 @@
 Module.register("MMM-moonphases.co.uk", {
   // Module config defaults.
   defaults: {
-      width: "95px",
-      height: "140px",
+      width: "175px",
+      height: "260px",
       updateInterval: 60 * 60 * 400, // 4 hours
   },
 
@@ -19,7 +19,7 @@ Module.register("MMM-moonphases.co.uk", {
     var wrapper = document.createElement("div");
 
     var frame = document.createElement("iframe");
-    frame.src = "https://tonymorris-mm.github.io/MMM-moonphases.co.uk/moon-phases.js";
+    frame.src = "https://tonymorris-mm.github.io/MMM-moonphases.co.uk/moon-phases.html";
     frame.class = "moon-phases-iframe";
     frame.allowtransparency = "true";
     frame.frameBorder = "0";
